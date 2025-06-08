a2562d3c1c04dd39812f6646719dce9a24056c54
Ugnius <ugnius.petryla@mif.stud.vu.lt>
2025-06-08 05:55:24 -0400

    add sbom to email

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -4,6 +4,8 @@ import json
 from email.message import EmailMessage
 from dotenv import load_dotenv
 
+SBOM_FILE_NAME = "sbom.json"
+
 # Load environment variables
 load_dotenv()
 
@@ -17,12 +19,20 @@ EMAIL_SUBJECT = "Vulnerability Scan"
 VILNERABILITY_REPORT_FILE_NAME = "vulnerability_report.json"
 
 
+def add_file_to_email(email, attachment_path):
+    with open(attachment_path, 'rb') as f:
+        email.add_attachment(f.read(), maintype='application', subtype = 'json', filename = os.path.basename(attachment_path))
+
+    return email
+
 def send_email(email_receiver, content):
     email = EmailMessage()
     email["Subject"] = EMAIL_SUBJECT
     email["To"] = email_receiver
     email["From"] = SMTP_EMAIL_FROM
     email.add_alternative(content, subtype = "html")
+
+    add_file_to_email(email, SBOM_FILE_NAME)
 
     with smtplib.SMTP(SMTP_SERVER_NAME, SMTP_PORT) as server:
         server.starttls()
